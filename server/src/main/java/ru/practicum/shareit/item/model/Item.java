919c3f58f6f3521c528cde9ec3ed419b3d4c485a
Elina1313 <112032648+Elina1313@users.noreply.github.com>
2023-06-04 17:40:19 +0300

    feat: add docker

---
+++ server/src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -0,0 +1,40 @@
+package ru.practicum.shareit.item.model;
+
+import lombok.*;
+import ru.practicum.shareit.request.model.ItemRequest;
+import ru.practicum.shareit.user.model.User;
+
+import javax.persistence.*;
+
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "items")
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", length = 255, nullable = false)
+    private String name;
+
+    @Column(name = "description", length = 255, nullable = false)
+    private String description;
+
+    @EqualsAndHashCode.Exclude
+    @Column(name = "is_available")
+    private Boolean available;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id", referencedColumnName = "id", nullable = false)
+    private User owner;
+
+    @ManyToOne
+    @JoinColumn(name = "request_id", referencedColumnName = "id")
+    private ItemRequest request;
+
+}
